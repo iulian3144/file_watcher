4e408c0df6098b759072c2d3d1b2b5f23b217bae
Iulian Paun <iulian.paun@cerner.com>
2019-05-28 10:53:29 +0300

    use logging in BaseAction

--- BaseAction.py
+++ BaseAction.py
@@ -1,5 +1,7 @@
+import logging
+
 class BaseAction:
     def __init__(self, arg):
         pass
     def invoke(self, fullpath, event_type, is_directory=False):
-        print(f'[{event_type}] Full file path: {fullpath}')
+        logging.info(f'[{event_type}] File path: {fullpath}')

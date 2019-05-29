9cfe56b2fbad09ce0ba82d32d283f3815ff0cb68
Iulian Paun <iulian.paun@cerner.com>
2019-05-29 17:09:50 +0300

    tweak message and add type annotations

--- BaseAction.py
+++ BaseAction.py
@@ -3,5 +3,6 @@ import logging
 class BaseAction:
     def __init__(self, arg):
         pass
-    def invoke(self, fullpath, event_type, is_directory=False):
-        logging.info(f'[{event_type}] File path: {fullpath}')
+    def invoke(self, fullpath: str, event_type: str, is_directory: bool=False):
+        item_type = "dir" if is_directory else "file"
+        logging.info(f'[{item_type:>4s} {event_type}] path: {fullpath}')

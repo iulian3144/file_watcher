0f155ae52aa3887b0c57bef157bdef0d55ef6779
Iulian Paun <iulian.paun@cerner.com>
2019-06-04 17:20:14 +0300

    refactor actions.py
    - include class name in the __init__ logs
    - use time.perf_counter() instead of time.time()
    - tweak copying progress messages

--- actions.py
+++ actions.py
@@ -38,26 +38,28 @@ class CopyAction(BaseAction):
     def __init__(self, dest: str):
         super().__init__(dest)
         if not (os.path.exists(dest) and os.path.isdir(dest)):
-            logging.error(f"[CopyAction] Passed argument ({dest}) is not a directory.")
+            logging.error(
+                f"[{self.__class__.__name__}] Passed argument ({dest}) is not a directory.")
             exit(1)
         self.destination = os.path.abspath(dest)
-        logging.info(f"[ACTION] Matched files will be copied to {self.destination}")
+        logging.info(
+            f"[{self.__class__.__name__}] Matched files will be copied to {self.destination}")
 
     def copy_file(self, fullpath):
         dirname, filename = os.path.split(fullpath)
         # quiet time
         time.sleep(1)
         logging.info(
-            f"Copy: <watch_path>\\{filename} to {self.destination}...")
-        t1 = time.time()
+            f"Copy START: <watch_path>\\{filename:40s} -> {self.destination}...")
+        t1 = time.perf_counter()
         dest_filename = os.path.join(self.destination, filename)
         copyfile(fullpath, dest_filename)
-        t2 = time.time()
+        t2 = time.perf_counter()
         time_total = t2 - t1
 
         size_auto = get_size_auto(fullpath)
         logging.info(
-            f"Done: <watch_path>\\{filename:40s} [{size_auto}] in {time_total:.3f}ms")
+            f" Copy DONE: <watch_path>\\{filename:40s} [{size_auto} in {time_total:.3f}s]")
 
     def invoke(self, fullpath: str, event_type: str, is_directory: bool = False):
         if not is_directory:
